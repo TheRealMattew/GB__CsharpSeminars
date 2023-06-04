59d83cbc89ea544261e2260faf6e543426172524
Admn <tst>
2023-06-04 15:49:29 +0300

    Folder for tests hs been created

---
+++ JustForTest/Program.cs
@@ -0,0 +1,22 @@
+﻿int x = 15;
+
+void Scaling(int num)
+{
+    int x = 2*num;
+}
+
+Scaling(x);
+
+Console.WriteLine(x); //15
+
+
+int x = 15;
+
+void Scaling(int num)
+{
+    x = 2*num;
+}
+
+Scaling(x);
+
+Console.WriteLine(x); //30
