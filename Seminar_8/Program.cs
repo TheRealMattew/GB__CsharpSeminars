aaa85aed57eba339a98a7c9fdd4e7c262d739f8a
Admn <tst>
2023-06-17 01:13:13 +0300

    homework for sem8 has been updated

--- Seminar_8/Program.cs
+++ Seminar_8/Program.cs
@@ -190,7 +190,7 @@ Console.WriteLine($"Ответ: Строка номер {m}");
 // -----------------------------------------------------------------------------------------------------------------
 // Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 // -----------------------------------------------------------------------------------------------------------------
-//*
+/*
 Console.WriteLine("Задача 58. || Программа находит произведение двух матриц.");
 
 
@@ -245,10 +245,156 @@ int[,] MatrixMultiplication(int[,] X, int[,] Y)
 int[,] A = Generate2DArray(2, 2, 0, 9);
 int[,] B = Generate2DArray(2, 3, 0, 2);
 Console.WriteLine();
-Console.WriteLine("Исходные массивы:");
+Console.WriteLine("Исходные матрицы:");
 Print2DArray(A);
 Print2DArray(B);
 int[,] R = MatrixMultiplication(A, B);
 Console.WriteLine("Ответ:");
 Print2DArray(R);
+//*/
+
+// -----------------------------------------------------------------------------------------------------------------
+// Задача 60:   Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет
+//              построчно выводить массив, добавляя индексы каждого элемента.
+// -----------------------------------------------------------------------------------------------------------------
+/*
+Console.WriteLine("Задача 60. || Программа генерирует трехмерный массив заданного размера с неповторяющимися 2значными числами.");
+
+int[,,] Generate3DArray(int rows, int columns, int layers)
+{
+    int[,,] result = new int[rows, columns, layers];
+    int[] numbers = new int[90];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = 10 + i;
+    }
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        int rand = new Random().Next(0, numbers.Length);
+        int temp = numbers[i];
+        numbers[i] = numbers[rand];
+        numbers[rand] = temp;
+    }
+
+    for (int i = 0; i < layers; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            for (int k = 0; k < columns; k++)
+            {
+                int pos = j*columns + k + i*rows*columns;
+                result[j, k, i] = numbers[pos];
+            }
+        }
+    }
+    return result;
+}
+
+void Print3DArrayWithIndex(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(2); i++)
+    {
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1); k++)
+            {
+                Console.Write($"{arr[j, k, i]}({j},{k},{i})  ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите кол-во строк: ");
+int arrRows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int arrColumns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во слоев: ");
+int arrLayers = Convert.ToInt32(Console.ReadLine());
+
+if( arrRows*arrColumns*arrLayers > 90)
+{   
+    Console.WriteLine("Матрицу такого размера нельзя сгенерировать");
+}
+else
+{
+    int[,,] R = Generate3DArray(arrRows, arrColumns, arrLayers);
+    Console.WriteLine("Ответ:");
+    Print3DArrayWithIndex(R);
+}
+
+//*/
+
+// -----------------------------------------------------------------------------------------------------------------
+// Задача 62:   Напишите программу, которая заполнит спирально массив 4 на 4.
+// -----------------------------------------------------------------------------------------------------------------
+//*
+Console.WriteLine("Задача 62. || Программа заполняет массив спиралью.");
+
+void GenerateSpiralArray(int[,] arr, int initialsizeX,int initialsizeY, int initialN)
+{
+    if (initialsizeX > 0 && initialsizeY > 0)
+    {
+        int X1 = (arr.GetLength(1)-initialsizeX)/2;
+        int Y1 = (arr.GetLength(0)-initialsizeY)/2;
+        int X2 = X1 + initialsizeX-1;
+        int Y2 = Y1;
+        int X3 = X2;
+        int Y3 = Y1 + initialsizeY-1;
+        int X4 = X1;
+        int Y4 = Y3;
+
+
+        for (int j = X1; j <= X2; j++)
+        {
+            arr[Y1, j] = initialN;
+            initialN++;
+        }
+
+        for (int i = Y2+1; i <= Y3; i++)
+        {
+            arr[i, X2] = initialN;
+            initialN++;
+        }
+
+        for (int j = X3-1; j >= X4; j--)
+        {
+            arr[Y3, j] = initialN;
+            initialN++;
+        }
+
+        for (int i = Y4-1; i >= Y1+1; i--)
+        {
+            arr[i, X4] = initialN;
+            initialN++;
+        }
+
+        GenerateSpiralArray(arr, initialsizeX-2, initialsizeY-2, initialN);
+    }
+}
+
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите кол-во строк: ");
+int arrRows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int arrColumns = Convert.ToInt32(Console.ReadLine());
+
+int[,] A = new int[arrRows, arrColumns];
+GenerateSpiralArray(A, A.GetLength(1), A.GetLength(0), 1);
+Print2DArray(A);
+
 //*/
